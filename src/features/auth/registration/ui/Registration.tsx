97561bad4e2c7686c5977215306dfb4b2f3cfc24
PetlitskayaVictoria <petlitskayavictoria@gmail.com>
2021-05-02 13:26:30 +0300

    #2 registration form: jsx

--- src/features/auth/registration/ui/Registration.tsx
+++ src/features/auth/registration/ui/Registration.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import SuperInputText from "../../../../main/ui/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../../../main/ui/common/c2-SuperButton/SuperButton";
+import styles from "./Registration.module.css"
 
 function Registration() {
     return (
-        <div>
+        <div className={styles.formContainer}>
             Registration
+            <SuperInputText />
+            <SuperInputText />
+            <SuperButton>Register</SuperButton>
         </div>
     );
 }
