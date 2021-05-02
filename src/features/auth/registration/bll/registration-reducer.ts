caa905c9b8f0ec0d00f95d10a0babf6df9e70093
PetlitskayaVictoria <petlitskayavictoria@gmail.com>
2021-05-02 14:34:53 +0300

    #2 add api, thunk

--- src/features/auth/registration/bll/registration-reducer.ts
+++ src/features/auth/registration/bll/registration-reducer.ts
@@ -1,16 +1,38 @@
-export type RegistrationActionType = {
-   type: string
-}
+import {Dispatch} from "react";
+import {registrationAPI} from "../dal/registration-api";
+import {Simulate} from "react-dom/test-utils";
 
-type ActionsType = RegistrationActionType
 type RegistrationStateType = {
+    isRegistered: boolean
 }
-const initialState: RegistrationStateType = {}
+const initialState: RegistrationStateType = {
+    isRegistered: false
+}
 
 export const registrationReducer = (state: RegistrationStateType = initialState, action: ActionsType): RegistrationStateType => {
     switch (action.type) {
-
         default:
             return state;
     }
 }
+
+const SetIsRegisteredAC = (isRegistered: boolean) => ({
+    type: SET_IS_REGISTERED,
+    isRegistered
+} as const)
+
+export const RegisterUserTC = (email: string, password: string) => (dispatch: Dispatch<any>) => {
+    registrationAPI.registerUser(email, password)
+        .then((res) => {
+            console.log(res.data)
+            SetIsRegisteredAC(true)
+        }).catch((error) => {
+        console.log(error)
+    })
+}
+
+const SET_IS_REGISTERED = 'SET_IS_REGISTERED'
+
+export type SetIsRegisteredACType = ReturnType<typeof SetIsRegisteredAC>
+
+type ActionsType = SetIsRegisteredACType
--- src/features/auth/registration/ui/Registration.tsx
+++ src/features/auth/registration/ui/Registration.tsx
@@ -2,11 +2,14 @@ import React, {useState} from "react";
 import SuperInputText from "../../../../main/ui/common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../../main/ui/common/c2-SuperButton/SuperButton";
 import styles from "./Registration.module.css"
+import {RegisterUserTC} from "../bll/registration-reducer";
+import {useDispatch} from "react-redux";
 
 function Registration() {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
 
     const onEmailChange = (value: string) =>  {
         setEmail(value)
@@ -14,13 +17,16 @@ function Registration() {
     const onPasswordChange = (value: string) => {
         setPassword(value)
     }
+    const registerUser = () => {
+        dispatch(RegisterUserTC(email, password))
+    }
 
     return (
         <div className={styles.formContainer}>
             Registration
             <SuperInputText onChangeText={onEmailChange}/>
             <SuperInputText onChangeText={onPasswordChange}/>
-            <SuperButton>Register</SuperButton>
+            <SuperButton onClick={registerUser}>Register</SuperButton>
         </div>
     );
 }
