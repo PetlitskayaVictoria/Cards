4b4a59c1c69a1c8482071513579b47a2c08217c9
PetlitskayaVictoria <petlitskayavictoria@gmail.com>
2021-05-02 13:50:25 +0300

    #2 add local state

--- src/features/auth/registration/ui/Registration.tsx
+++ src/features/auth/registration/ui/Registration.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import SuperInputText from "../../../../main/ui/common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../../main/ui/common/c2-SuperButton/SuperButton";
 import styles from "./Registration.module.css"
 
 function Registration() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onEmailChange = (value: string) =>  {
+        setEmail(value)
+    }
+    const onPasswordChange = (value: string) => {
+        setPassword(value)
+    }
+
     return (
         <div className={styles.formContainer}>
             Registration
-            <SuperInputText />
-            <SuperInputText />
+            <SuperInputText onChangeText={onEmailChange}/>
+            <SuperInputText onChangeText={onPasswordChange}/>
             <SuperButton>Register</SuperButton>
         </div>
     );
