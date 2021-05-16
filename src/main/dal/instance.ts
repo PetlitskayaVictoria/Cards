98c78c32e3981c0cd31d050863c684a4e90da3af
PetlitskayaVictoria <petlitskayavictoria@gmail.com>
2021-05-16 16:15:19 +0300

    fix instance

--- src/main/dal/instance.ts
+++ src/main/dal/instance.ts
@@ -3,5 +3,4 @@ import axios from "axios";
 export const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://neko-back.herokuapp.com/2.0'
-    // baseURL: 'http://localhost:7542/2.0/'
 })
